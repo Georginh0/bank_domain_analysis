ecbdd016b95712a11d99308c758f9b68c2dcaab1
Georginh0 <george.sam@live.co.uk>
2026-02-03 18:23:10 +0100

    Initial commit

---
+++ banking_case.sql
@@ -0,0 +1,9 @@
+	CREATE database banking_case;
+	DROP DATABASE banking_case;
+CREATE DATABASE banking_case;
+
+ALTER DATABASE banking_case
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+    
